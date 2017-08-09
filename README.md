35959bdd543e63263f4d0fe2e659f01b9c93d24c
JianmingXia <1179801728@qq.com>
2017-08-09 23:38:16 +0800

    webpack 打包第三方类库 html-wepack-plugin 使用（目前还要一些问题）

--- README.md
+++ README.md
@@ -4,13 +4,21 @@
 ### 准备工作
 - npm install ts-loader --save-dev
 - npm install typescript
-- npm install awesome-typescript-loader
+- npm install awesome-typescript-loader (未使用)
+- npm install html-webpack-plugin --save-dev
+- npm install extract-text-webpack-plugin --save-dev
+再同package.json比对
 
 ### Log
  1.模块化打包
+ 2.htmlWebpackPlugin插件
+ 3.webpack打包第三方类库 eg: lodash
 
 ### 修改部分
   "dependencies": {
     "@types/lodash": "4.14.44",
     "lodash": "^4.17.4"
   }
+
+### 注意
+之前全局安装的webpack 1.13.x版本的，这次打包时出现了问题（目前在项目安装的webpack是3.5.2，可正常打包）
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,58 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var publicPath = 'http://127.0.0.1:20006/dist_web/';
+
 var entry = {
   app: "./src/index.ts",
-  // vendor: [
-  //   "lodash"
-  // ]
+  vendor: [
+    "lodash"
+  ]
 };
+var plugins = [
+  // new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
+  // new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename: "vendor.bundle.js" })
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.[hash].min.js',
+  }),
+  // new ExtractTextPlugin('css/[contenthash].css', {
+  //   allChunks: true
+  // }),
+  new HtmlWebpackPlugin({
+    title: 'TypeScript Demo',
+    template: './src/index.html'
+  })
+];
+
 
 module.exports = {
-  entry: entry,
+  entry: "./src/index.ts",
   output: {
-    filename: "./dist_web/bundle.js"
+    // filename: "./dist_web/bundle.js"
+    // filename: '[name].js',
+    filename: 'module.[hash].js',
+    path: __dirname + '/dist_web',
+    publicPath: publicPath,
+    libraryTarget: 'umd'
   },
+  plugins: plugins,
   resolve: {
     // Add '.ts' and '.tsx' as a resolvable extension.
     extensions: [".ts", ".tsx", ".js"]
   },
   module: {
     loaders: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        // loader: "awesome-typescript-loader",
-        exclude: /(node_modules)/
-      }
-    ]
+    //   {
+    //   test: /\.html$/,
+    //   loader: 'html-loader'
+    // },
+    {
+      test: /\.tsx?$/,
+      loader: "ts-loader",
+      // loader: "awesome-typescript-loader",
+      exclude: /(node_modules)/
+    }]
   }
 };
